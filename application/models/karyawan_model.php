b57f8972317432df63d91ebef62c7e5706e798ea
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-20 15:05:54 +0700

    membuat file karyawan _model.php

---
+++ application/models/karyawan_model.php
@@ -0,0 +1,13 @@
+<?php 
+
+    class Karyawan_model extends CI_Model {
+
+        public function semua() 
+        {
+            $query =$this->db->select('*')
+                            ->get('karyawan');
+            
+            return $query;
+        }
+
+    }
