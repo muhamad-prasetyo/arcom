5390108053fdffed2bc087963aba28190d816c21
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-21 09:53:26 +0700

    menambahkan route code pada file routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,4 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'halaman/tampil';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = TRUE;
+
+// route untuk form register 
+$route['register'] = 'user/register';
+$route['register/proses'] = 'user/prosesRegister';
